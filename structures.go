20b27b47225fb3d2114449979ce3db82db797f34
Nick Landolfi <nclandolfi@gmail.com>
2015-09-19 21:19:41 -0700

    add several social welfare functions

--- structures.go
+++ structures.go
@@ -8,6 +8,10 @@ type (
 	Alternatives Set
 
 	Preference BinaryRelation
+
+	PreferenceProfile []Preference
+
+	SocialWelfareFunction func(PreferenceProfile) Preference
 )
 
 // --- }}
@@ -16,6 +20,102 @@ type (
 
 func Rational(p Preference) bool {
 	return WeakOrder(p)
+}
+
+func ComposablePreferences(prefs []Preference) bool {
+	br := make([]BinaryRelation, len(prefs))
+
+	for i := range prefs {
+		br[i] = BinaryRelation(prefs[i])
+	}
+
+	return ComposableRelations(br)
+}
+
+func ProfileUniverse(p PreferenceProfile) Alternatives {
+	assert(len(p) > 0, "preference profile empty")
+	assert(ComposablePreferences(p), "preferneces not defined over same universe")
+
+	return p[0].Universe()
+}
+
+func CountPreferenceOf(x, y Alternative, pp PreferenceProfile) float64 {
+	return CountWeightedPreferenceOf(x, y, pp, func(i uint) float64 { return 1 })
+}
+
+func CountWeightedPreferenceOf(x, y Alternative, pp PreferenceProfile, w func(uint) float64) float64 {
+	c := 0.0
+
+	for i, p := range pp {
+		if p.ContainsRelation(x, y) {
+			c += w(uint(i))
+		}
+	}
+
+	return c
+}
+
+func BordaCount(x Alternative, p Preference) uint {
+	c := uint(0)
+
+	for e := range p.Universe().Elements() {
+		if p.ContainsRelation(x, e) {
+			c += 1
+		}
+	}
+
+	return c
+}
+
+func ProfileBordaCount(x Alternative, pp PreferenceProfile) uint {
+	c := uint(0)
+
+	for _, p := range pp {
+		c += BordaCount(x, p)
+	}
+
+	return c
+}
+
+// --- }}}
+
+// --- Social Welfare Function Implementations {{{
+
+func PairwiseMajority(pp PreferenceProfile) Preference {
+	u := ProfileUniverse(pp)
+
+	return NewFunctionBinaryRelation(u, func(x, y Element) bool {
+		return CountPreferenceOf(x, y, pp) >= CountPreferenceOf(y, x, pp)
+	})
+}
+
+func BordaCounting(pp PreferenceProfile) Preference {
+	u := ProfileUniverse(pp)
+
+	return NewFunctionBinaryRelation(u, func(x, y Element) bool {
+		return ProfileBordaCount(x, pp) >= ProfileBordaCount(y, pp)
+	})
+}
+
+func Dictatorship(pp PreferenceProfile, individual uint) Preference {
+	return pp[individual]
+}
+
+func AntiDictatorship(pp PreferenceProfile, individual uint) Preference {
+	return Reverse(pp[individual])
+}
+
+func Constant(pp PreferenceProfile, actual Preference) Preference {
+	// interesting how explicit it become that a constant one depends not on the PreferenceProfile
+	return actual
+}
+
+func WeightedMajority(pp PreferenceProfile, w func(uint) float64) Preference {
+	u := ProfileUniverse(pp)
+
+	return NewFunctionBinaryRelation(u, func(x, y Element) bool {
+		return CountWeightedPreferenceOf(x, y, pp, w) >= CountWeightedPreferenceOf(y, x, pp, w)
+	})
 }
 
 // --- }}}
